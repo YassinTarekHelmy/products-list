2ff4442799319c2ac0015bb5fb3de856a8670054
Yassin Tarek Helmy <102481887+YassinTarekHelmy@users.noreply.github.com>
2025-04-16 23:33:40 +0200

    final update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Total from './components/total/total'
 class App extends Component {
   state = {
     items: [
-      {id:1, product:'Pen', price:2, quantity:1},
-      {id:2, product:'Book', price:10, quantity:1}
+      {id:1, product:'Pen', price:2, quantity:1 },
+      {id:2, product:'Book', price:10, quantity:1 }
     ]
   }
 
